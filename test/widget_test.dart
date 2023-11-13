0be985f983ac8a36197d3e01ca194ecee3ec4d8b
Naufal Rozan <naufal.rozan.ft20@mail.umy.ac.id>
2023-11-13 13:44:34 +0700

    Unit testing fitur Delete

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:notes_app/main.dart';
 
@@ -8,5 +9,37 @@ void main() {
 
     // Verify that our NoteList widget is rendered.
     expect(find.byType(NoteList), findsOneWidget);
+  });
+
+  testWidgets('Adds and deletes a note', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(SimpleNotesApp());
+
+    // Verify that the NoteList widget initially has no notes.
+    expect(find.byType(ListTile), findsNothing);
+
+    // Tap the add note button.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pumpAndSettle();
+
+    expect(find.text('Add New Note'), findsOneWidget);
+
+    // Enter note details and tap the save button.
+    await tester.enterText(
+        find.widgetWithText(TextField, 'Title'), 'Test Title');
+    await tester.enterText(
+        find.widgetWithText(TextField, 'Contents of Note'), 'Test Content');
+    await tester.tap(find.text('Save'));
+    await tester.pump();
+
+    // Verify that the NoteList widget now has one note.
+    expect(find.byType(ListTile), findsOneWidget);
+
+    // Tap the delete icon.
+    await tester.tap(find.byIcon(Icons.delete));
+    await tester.pump();
+
+    // Verify that the NoteList widget no longer has any notes.
+    expect(find.byType(ListTile), findsNothing);
   });
 }
